3974dc5fca6769ef1106404e9e8876d166728218
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-10 19:40:18 -0300

    feat(UI): add custom color pallet

---
+++ src/main/java/org/ichnaea/core/ui/semantic/SemanticColor.kt
@@ -0,0 +1,18 @@
+package org.ichnaea.core.ui.semantic
+
+import java.awt.Color
+
+class SemanticColor {
+
+    companion object {
+        val PRIMARY = Color.decode("#0d6efd")
+        val SECONDARY = Color.decode("#6c757d")
+        val SUCCESS = Color.decode("#198754")
+        val DANGER = Color.decode("#dc3545")
+        val WARNING = Color.decode("#ffc107")
+        val INFO = Color.decode("#0dcaf0")
+        val LIGHT = Color.decode("#f8f9fa")
+        val DARK = Color.decode("#212529")
+    }
+
+}
