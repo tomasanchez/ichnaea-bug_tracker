f890e145be535aa4c100ca2ea2b0ac1e21868aba
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-13 16:15:03 -0300

    feat(UI): add TransparentPanel

---
+++ src/main/java/org/ichnaea/core/ui/container/TransparentPanel.kt
@@ -0,0 +1,21 @@
+package org.ichnaea.core.ui.container
+
+import java.awt.AlphaComposite
+import java.awt.Graphics
+import java.awt.Graphics2D
+import javax.swing.JPanel
+
+
+class TransparentPanel(var alpha: Float = 1f) : JPanel() {
+
+    init {
+        isOpaque = false
+    }
+
+    override fun paint(grphcs: Graphics) {
+        val g2 = grphcs as Graphics2D
+        g2.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha)
+        super.paint(grphcs)
+    }
+    
+}
