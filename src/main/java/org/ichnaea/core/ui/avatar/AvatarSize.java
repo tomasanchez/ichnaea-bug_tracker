8ee93e48cec8ebe021f88afa1dfc9f021f57d95f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-17 14:49:47 -0300

    refactor(UI): make AvatarSize public

--- src/main/java/org/ichnaea/core/ui/avatar/AvatarSize.java
+++ src/main/java/org/ichnaea/core/ui/avatar/AvatarSize.java
@@ -4,13 +4,13 @@ import java.awt.*;
 
 public class AvatarSize {
 
-    static final Dimension XS = new Dimension(32, 32);
-    static final Dimension S = new Dimension(64, 64);
+    public static final Dimension XS = new Dimension(32, 32);
+    public static final Dimension S = new Dimension(64, 64);
 
-    static final Dimension M = new Dimension(128, 128);
+    public static final Dimension M = new Dimension(128, 128);
 
-    static final Dimension L = new Dimension(256, 256);
+    public static final Dimension L = new Dimension(256, 256);
 
-    static final Dimension XL = new Dimension(512, 512);
+    public static final Dimension XL = new Dimension(512, 512);
 
 }
