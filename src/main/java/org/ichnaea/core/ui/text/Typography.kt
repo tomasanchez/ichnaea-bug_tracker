ac7067aa47aad69037700ba7fa37e214511b47a9
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-20 17:01:55 -0300

    refactor(UI): add html support

--- src/main/java/org/ichnaea/core/ui/text/Typography.kt
+++ src/main/java/org/ichnaea/core/ui/text/Typography.kt
@@ -22,15 +22,17 @@ open class Typography(
     ha: Float = CENTER_ALIGNMENT,
 
     style: Int = Font.PLAIN,
+
+    size: Float = 14f,
 ) : JLabel() {
 
     init {
         foreground = color
         alignmentX = va
         alignmentY = ha
-        font = font.deriveFont(style, 14f)
+        font = font.deriveFont(style, size)
         border = EmptyBorder(0, 3, 0, 3)
-        this.text = text
+        this.text = "<html>$text</html>"
     }
 
 
