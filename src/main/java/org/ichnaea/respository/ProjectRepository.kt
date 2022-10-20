af32cd6903bc5bfb57fa9ae27c5b724dc0aa616c
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-20 18:43:10 -0300

    chore(mvc): add dummy data

--- src/main/java/org/ichnaea/respository/ProjectRepository.kt
+++ src/main/java/org/ichnaea/respository/ProjectRepository.kt
@@ -6,19 +6,23 @@ import org.ichnaea.model.User
 object ProjectRepository : CrudRepository<Project>() {
     init {
 
+        val owner = User("admin", "")
+
         save(
             Project(
                 name = "Ichnaea",
-                description = "Description 1",
-                owner = User("admin", ""),
+                description = "It is a Bug Tracker Project which has many bugs! " +
+                        "Ironic. It could save others from bugs, but not itself.",
+                owner = owner,
             )
         )
 
         save(
             Project(
                 name = "Code for Good",
-                description = "Description 1",
-                owner = User("admin", ""),
+                description = "Showcase your tech skills and work alongside a team, guided by our technologists," +
+                        " to solve real-world problems for social good organizations.",
+                owner = owner,
             )
         )
 
