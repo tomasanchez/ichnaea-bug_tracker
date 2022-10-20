01fe338ee728d3ba9460f07e3739bd7654373ce6
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-20 17:01:38 -0300

    feat(UI): add Tabbed Container

---
+++ src/main/java/org/ichnaea/core/ui/container/TabContainer.java
@@ -0,0 +1,151 @@
+package org.ichnaea.core.ui.container;
+
+import org.ichnaea.core.ui.semantic.SemanticColor;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.interpolation.PropertySetter;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.metal.MetalTabbedPaneUI;
+import java.awt.*;
+
+public class TabContainer extends JTabbedPane {
+
+    public TabContainer() {
+        setUI(new MaterialTabbedUI());
+    }
+
+    @Override
+    public Color getForegroundAt(int index) {
+        return getSelectedIndex() == index ? SemanticColor.PRIMARY : SemanticColor.SECONDARY;
+    }
+
+    public class MaterialTabbedUI extends MetalTabbedPaneUI {
+
+        private Animator animator;
+        private Rectangle currentRectangle;
+        private TimingTarget target;
+
+        public MaterialTabbedUI() {
+        }
+
+        public void setCurrentRectangle(Rectangle currentRectangle) {
+            this.currentRectangle = currentRectangle;
+            repaint();
+        }
+
+        @Override
+        public void installUI(JComponent jc) {
+            super.installUI(jc);
+            animator = new Animator(500);
+            animator.setResolution(0);
+            animator.setAcceleration(.5f);
+            animator.setDeceleration(.5f);
+            tabPane.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent ce) {
+                    int selected = tabPane.getSelectedIndex();
+                    if (selected != -1) {
+                        if (currentRectangle != null) {
+                            if (animator.isRunning()) {
+                                animator.stop();
+                            }
+                            animator.removeTarget(target);
+                            target = new PropertySetter(MaterialTabbedUI.this, "currentRectangle",
+                                    currentRectangle, getTabBounds(selected, calcRect));
+                            animator.addTarget(target);
+                            animator.start();
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        protected void installDefaults() {
+            super.installDefaults();
+            selectColor = Color.WHITE;
+        }
+
+        @Override
+        protected void paintTabBackground(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h, boolean isSelected) {
+            super.paintTabBackground(g, tabPlacement, tabIndex, x, y, w, h, isSelected);
+
+            if (isSelected) {
+                g.setColor(selectColor);
+                g.fillRect(x, y, w, h);
+            } else {
+                g.setColor(SemanticColor.LIGHT);
+                g.fillRect(x, y, w, h);
+            }
+        }
+
+        @Override
+        protected Insets getTabInsets(int i, int i1) {
+            return new Insets(10, 10, 10, 10);
+        }
+
+        @Override
+        protected void paintTabBorder(Graphics grphcs, int tabPlacement, int tabIndex, int x, int y,
+                                      int w, int h, boolean isSelected) {
+            Graphics2D g2 = (Graphics2D) grphcs.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(SemanticColor.PRIMARY);
+
+            if (currentRectangle == null || !animator.isRunning()) {
+                if (isSelected) {
+                    currentRectangle = new Rectangle(x, y, w, h);
+                }
+            }
+
+            if (currentRectangle != null) {
+                if (tabPlacement == TOP) {
+                    g2.fillRect(currentRectangle.x, currentRectangle.y + currentRectangle.height - 3,
+                            currentRectangle.width, 3);
+                } else if (tabPlacement == BOTTOM) {
+                    g2.fillRect(currentRectangle.x, currentRectangle.y, currentRectangle.width, 3);
+                } else if (tabPlacement == LEFT) {
+                    g2.fillRect(currentRectangle.x + currentRectangle.width - 3, currentRectangle.y, 3,
+                            currentRectangle.height);
+                } else if (tabPlacement == RIGHT) {
+                    g2.fillRect(currentRectangle.x, currentRectangle.y, 3, currentRectangle.height);
+                }
+            }
+
+            g2.dispose();
+        }
+
+        @Override
+        protected void paintContentBorder(Graphics grphcs, int tabPlacement, int selectedIndex) {
+            Graphics2D g2 = (Graphics2D) grphcs.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(new Color(200, 200, 200));
+            Insets insets = getTabAreaInsets(tabPlacement);
+            int width = tabPane.getWidth();
+            int height = tabPane.getHeight();
+            if (tabPlacement == TOP) {
+                int tabHeight = calculateTabAreaHeight(tabPlacement, runCount, maxTabHeight);
+                g2.drawLine(insets.left, tabHeight, width - insets.right - 1, tabHeight);
+            } else if (tabPlacement == BOTTOM) {
+                int tabHeight = height - calculateTabAreaHeight(tabPlacement, runCount, maxTabHeight);
+                g2.drawLine(insets.left, tabHeight, width - insets.right - 1, tabHeight);
+            } else if (tabPlacement == LEFT) {
+                int tabWidth = calculateTabAreaWidth(tabPlacement, runCount, maxTabWidth);
+                g2.drawLine(tabWidth, insets.top, tabWidth, height - insets.bottom - 1);
+            } else if (tabPlacement == RIGHT) {
+                int tabWidth = width - calculateTabAreaWidth(tabPlacement, runCount, maxTabWidth) - 1;
+                g2.drawLine(tabWidth, insets.top, tabWidth, height - insets.bottom - 1);
+            }
+            g2.dispose();
+        }
+
+        @Override
+        protected void paintFocusIndicator(Graphics grphcs, int i, Rectangle[] rctngls, int i1, Rectangle rctngl,
+                                           Rectangle rctngl1, boolean bln) {
+        }
+
+    }
+
+}
