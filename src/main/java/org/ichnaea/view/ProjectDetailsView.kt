a511a0803f52e44d309a134c2cebdea62b3b320e
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-20 18:43:45 -0300

    refactor(mvc): add tab container

--- src/main/java/org/ichnaea/view/ProjectDetailsView.kt
+++ src/main/java/org/ichnaea/view/ProjectDetailsView.kt
@@ -1,7 +1,10 @@
 package org.ichnaea.view
 
+import jiconfont.icons.google_material_design_icons.GoogleMaterialDesignIcons
 import net.miginfocom.swing.MigLayout
 import org.ichnaea.core.mvc.view.UIView
+import org.ichnaea.core.ui.container.TabContainer
+import org.ichnaea.core.ui.icon.GoogleIconFactory
 import org.ichnaea.core.ui.semantic.SemanticColor
 import org.ichnaea.core.ui.text.Title
 import org.ichnaea.core.ui.text.TitleLevel
@@ -9,17 +12,27 @@ import org.ichnaea.core.ui.text.Typography
 import org.ichnaea.model.Project
 import java.awt.Dimension
 import javax.swing.Box
+import javax.swing.JPanel
 
 @UIView
 class ProjectDetailsView : SideView() {
+
+    private var project: Project? = null
+
+    private lateinit var tabs: TabContainer
 
     init {
         onBeforeRendering()
     }
 
     private fun onBeforeRendering() {
-        containerPanel.layout = MigLayout("debug")
+        containerPanel.layout = MigLayout(
+            "fill",
+            "0[]10[100%, fill]0",
+            "0[fill, top]0"
+        )
         header()
+        body()
     }
 
     override fun repaint() {
@@ -32,21 +45,55 @@ class ProjectDetailsView : SideView() {
     // View Drawing
     // ---------------------------------------------------------------------------------------------
 
-    fun header() {
+    private fun header() {
 
-        val project: Project? = model["project"] as Project?
+        project = model["project"] as Project?
+
         project?.let {
 
             val title = Title(text = it.name, level = TitleLevel.H3)
-            containerPanel.add(title, "align center")
-            containerPanel.add(Box.createRigidArea(Dimension(200, 0)), "align center, w 300!, growx")
-            containerPanel.add(Typography(text = it.creationDate), "wrap, align center")
-
+            containerPanel.add(title, "align left, wrap")
             val description =
                 Typography(text = it.description, color = SemanticColor.DARK.brighter())
-            containerPanel.add(description, "align right, wrap")
+            containerPanel.add(description, "align right, growx, wrap")
+            val date = Typography(text = "<b>Created at:</b> ${it.creationDate}", size = 12f)
+            containerPanel.add(date, "align right, growx, wrap")
         }
 
+    }
+
+    private fun body() {
+
+        containerPanel.add(
+            Box.createRigidArea(
+                Dimension(200, 50)
+            ),
+            "align center, w 300!, growx, wrap"
+        )
+
+        createTab()
+        containerPanel.add(tabs, "align center, growx,  spanx, wrap")
+
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Components
+    // ---------------------------------------------------------------------------------------------
+
+    private fun createTab() {
+        tabs = TabContainer()
+        issueTab()
+        tabs.addTab("Members", JPanel())
+    }
+
+    private fun issueTab() {
+        val tab = JPanel().also { it.background = SemanticColor.LIGHT }
+        val icon = GoogleIconFactory.build(
+            name = GoogleMaterialDesignIcons.BUG_REPORT,
+            color = SemanticColor.DARK,
+            size = 24f
+        )
+        tabs.addTab("Issues", icon, tab)
     }
 
 
