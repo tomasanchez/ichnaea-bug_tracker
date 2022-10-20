a72cf8dda013c8cf3ee0acaef5bb98e282b5471b
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-20 13:52:40 -0300

    refactor(UI): moved to JAVA for better compatibility

---
+++ src/main/java/org/ichnaea/core/ui/semantic/SemanticColor.java
@@ -0,0 +1,17 @@
+package org.ichnaea.core.ui.semantic;
+
+import java.awt.*;
+
+public class SemanticColor {
+
+    public static final Color PRIMARY = Color.decode("#3B71CA");
+    public static final Color SECONDARY = Color.decode("#6c757d");
+    public static final Color SUCCESS = Color.decode("#198754");
+    public static final Color DANGER = Color.decode("#dc3545");
+    public static final Color WARNING = Color.decode("#ffc107");
+    public static final Color INFO = Color.decode("#0dcaf0");
+
+    public static final Color LIGHT = Color.decode("#FBFBFB");
+    
+    public static final Color DARK = Color.decode("#212529");
+}
