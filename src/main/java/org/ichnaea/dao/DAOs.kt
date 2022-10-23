044ecfb5f569eb434c3a406c807614d0ad91fd6b
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-10-23 20:50:13 -0300

    refactor(DAO): update ProjectDAO interface

--- src/main/java/org/ichnaea/dao/DAOs.kt
+++ src/main/java/org/ichnaea/dao/DAOs.kt
@@ -1,5 +1,6 @@
 package org.ichnaea.dao
 
+import org.ichnaea.model.Project
 import org.ichnaea.model.Role
 import org.ichnaea.model.User
 import java.util.*
@@ -8,4 +9,40 @@ interface RoleDAO : PersistentEntityDAO<Role>
 
 interface UserDAO : PersistentEntityDAO<User> {
     fun findByUsername(username: String): Optional<User>
+}
+
+interface ProjectDAO : PersistentEntityDAO<Project> {
+
+    /**
+     * Find all project members
+     *
+     * @param id of the project
+     * @return a list of user ids
+     */
+    fun findMembers(id: Long): List<Long>
+
+    /**
+     * Add user to project
+     *
+     * @param userId the user unique identifier
+     * @param projectId the project unique identifier
+     */
+    fun addUser(userId: Long, projectId: Long)
+
+    /**
+     * Removes a user from a project
+     *
+     * @param userId the user unique identifier
+     * @param projectId the project unique identifier
+     */
+    fun removeUser(userId: Long, projectId: Long)
+
+    /**
+     * Find projects of a user
+     *
+     * @param userId to be searched
+     * @return a list of project in which the user is a member
+     */
+    fun findProjects(userId: Long): List<Project>
+
 }
